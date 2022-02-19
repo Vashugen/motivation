f0fa8c6e9b01d88518145caf2baf3df4b6ca83f4
VashuGen <vashu_gen@mail.ru>
2022-02-19 20:06:51 +0300

    work with prizes

--- frontend/views/page/common/login/prize.php
+++ frontend/views/page/common/login/prize.php
@@ -1,34 +1,22 @@
 <?php
-$this->registerJsFile('/js/common.js');
+$this->registerJsFile('/js/common.js', ['depends' => [\yii\web\JqueryAsset::className(),\yii\bootstrap4\BootstrapAsset::className()]/*, 'position' => yii\web\View::POS_LOAD*/]);
 ?>
 
 <style>
 
-    .div-text
-
     .prize-input {
+        margin-top: 1%;
+        font-size: 1.7em;
         border-radius: 8px;
     }
 
-    .div-input {
-        margin-top: 1%;
-        font-size: 2em;
+    .prize-col {
+        float: left;
+        /*margin: 0 0 0 3.5%;*/
     }
 
-    .div-prize-btn {
-        margin-top: 50px;
-    }
-
-    .btn-login {
-        display: inline-block; /* строчно-блочный */
-        padding: 20px 60px; /* расстояние от текста до края кнопки */
-        background-color: #00e4a2; /* цвет кнопки  */
-        border-radius: 20px; /* скругление углов */
-        font-size: 50px; /* размер шрифта */
-        text-transform: uppercase; /* трансформировать в заглавные буквы */
-        text-decoration: none; /* не подчеркивать ссылку */
-        transition: 0.2s; /* плавный ховер эффект */
-        color: #f7f8e8; /* цвет текста на кнопке */
+    .prize-col:first-child {
+        margin-left: 0;
     }
 
     input::placeholder {
@@ -54,11 +42,23 @@ $this->registerJsFile('/js/common.js');
     }
 
     .desc {
-
+        font-weight: normal;
     }
 
     .inner-container {
         margin-top: 2%;
+    }
+
+    .prize-btn {
+        display: inline-block; /* строчно-блочный */
+        padding: 10px 30px; /* расстояние от текста до края кнопки */
+        background-color: #00e4a2; /* цвет кнопки  */
+        border-radius: 20px; /* скругление углов */
+        font-size: 40px; /* размер шрифта */
+        text-transform: uppercase; /* трансформировать в заглавные буквы */
+        text-decoration: none; /* не подчеркивать ссылку */
+        transition: 0.2s; /* плавный ховер эффект */
+        color: #f7f8e8; /* цвет текста на кнопке */
     }
 
 </style>
@@ -69,27 +69,76 @@ $this->registerJsFile('/js/common.js');
 <div class="container inner-container text desc">
     <div>
         Настало время выбрать свою награду за прохождение ключевых шагов!<br>
-        Впишите в поля свои собственные варианты наград или перетащите предложенные ниже картинки!<br>
+        Впишите в поля свои собственные варианты наград или отметьте что-то из предложенных ниже картинок!<br>
         В ключевой момент мы напомним тебе о выбранной тобой награде!)<br>
     </div>
 </div>
-<div class="container inner-container prize-item">
-    <div class="div-input" align="center">
-        <input type="text" class="login-input" name="userName" placeholder="ВАШЕ ИМЯ" autocomplete="off">
+<form id="prizeForm">
+    <div id="prizePics" hidden>
+        <input type="text" hidden id="prizePicsOne" value="">
+        <input type="text" hidden id="prizePicsTwo" value="">
+        <input type="text" hidden id="prizePicsThree" value="">
     </div>
-    <div class="div-input" align="center">
-        <input type="text" class="login-input" name="userLogin" placeholder="ЛОГИН" autocomplete="off">
+    <div class="container inner-container prize-item" style="padding: 10px">
+        <div class="row">
+            <div class="col-lg-4">
+                <input type="text" class="prize-input" name="prizeOne" placeholder="НАГРАДА" autocomplete="off">
+            </div>
+            <div class="col-lg-4 col-lg-offset-1">
+                <input type="text" class="prize-input" name="prizeTwo" placeholder="НАГРАДА" autocomplete="off">
+            </div>
+            <div class="col-lg-4">
+                <input type="text" class="prize-input" name="prizeThree" placeholder="НАГРАДА" autocomplete="off">
+            </div>
+        </div>
     </div>
-    <div class="div-input" align="center">
-        <input type="text" class="login-input" name="userPassword" placeholder="ПАРОЛЬ" autocomplete="off">
+    <div class="container inner-container">
+        <div class="row">
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/cake.png" class="prize-picture" alt="любимый десерт" id="prizePicCake" name="prizePicCake" width="100%" data-checked="0">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/vine.png" alt="" class="prize-picture" id="prizePicVine" name="prizePicVine" width="100%" data-checked="0">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/ring.png" alt="" width="100%" class="prize-picture" id="prizePicRing" name="prizePicRing" data-checked="0">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/car.png" alt="" width="100%" class="prize-picture" id="prizePicCar" name="prizePicCar" data-checked="0">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="" alt="" id="prizePicsOne" name="prizePicsOne" width="100%">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="" alt="" id="prizePicsOne" name="prizePicsOne" width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/cake.png" alt="любимый десерт" id="prizePicCake" name="prizePicCake" width="100%">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/vine.png" alt="" id="prizePicVine" name="prizePicVine" width="100%">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="<?=Yii::$app->request->baseUrl;?>/images/ring.png" alt="" width="100%" id="prizePicRing" name="prizePicRing">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="" alt="" id="prizePicsOne" name="prizePicsOne" width="100%">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="" alt="" id="prizePicsOne" name="prizePicsOne" width="100%">
+            </div>
+            <div class="col-md-2 prize-col">
+                <img src="" alt="" id="prizePicsOne" name="prizePicsOne" width="100%">
+            </div>
+        </div>
     </div>
-</div>
-<!--<div class="container inner-container prize-pic">
-    тут будет блок с картинками
-</div>-->
-<div class="container prize-btn inner-container">
-    <div class="" align="right">
-        <button type="submit" class="login-button">Сохранить</button>
+    <div class="container inner-container">
+        <div class="" align="right">
+            <button type="submit" class="prize-btn">Сохранить</button>
+        </div>
     </div>
-</div>
+</form>
+
 
--- frontend/web/js/common.js
+++ frontend/web/js/common.js
@@ -1,3 +1,15 @@
-$("#loginForm").on("submyt", function () {
-   alert("HERE");
+$("#prizeForm").on("submit", function (){
+
+   alert("here in prize");
+
+});
+
+$(".prize-picture").on("click", function (){
+
+   var element = $(this);
+   //0. определяем, чекнута картинка или нет
+   //1. определяем, есть ли ещё места
+   //2.вставляем в свободный инпут
+   //3.заменяем картинку
+
 });
